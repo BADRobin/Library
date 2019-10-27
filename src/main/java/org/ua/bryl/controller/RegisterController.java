bc7e52478b7f3a6c29d00650402bf1a2abc241d7
BADRobin <olegbryl@gmail.com>
2019-10-27 11:49:18 +0200

    upload image and file

--- src/main/java/org/ua/bryl/controller/RegisterController.java
+++ src/main/java/org/ua/bryl/controller/RegisterController.java
@@ -4,13 +4,15 @@ import org.ua.bryl.model.People;
 import org.ua.bryl.services.PeopleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+//import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 
 import javax.validation.Valid;
 import java.util.List;
